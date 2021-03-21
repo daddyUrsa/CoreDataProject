87766e3d1fbf05eb93b7ecbe21409c471a0a8ff6
daddyUrsa <0057747@gmail.com>
2021-03-21 20:36:14 +0300

    Initial Commit

---
+++ CoreDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataProject
+//
+//  Created by Alexey Golovin on 21.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

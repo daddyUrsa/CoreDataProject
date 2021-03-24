90007bfcd420872b8ca704b9c831dca82af9fddc
daddyUrsa <0057747@gmail.com>
2021-03-24 20:43:52 +0300

    Add constants.

---
+++ CoreDataProject/Helpers/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  CoreDataProject
+//
+//  Created by Alexey Golovin on 24.03.2021.
+//
+
+import UIKit
+
+struct Constants {
+    static let padding: CGFloat = 16
+    static let imageSize: CGFloat = 150
+    static let playerNumberLabel: CGFloat = 30
+    static let playerImage: CGFloat = 150
+    static let teamLabel: CGFloat = 120
+    static let playerViewImage: CGFloat = 200
+    static let textFieldWidth: CGFloat = 300
+    static let playerNumberTF: CGFloat = 30
+    static let textFieldBottom: CGFloat = 10
+    static let textFieldLeading: CGFloat = 30
+    static let labelBottom: CGFloat = 20
+}
